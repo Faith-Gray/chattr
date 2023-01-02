6db4166a4f1ed25833dad8eda436a1907713d896
Faith <fhgray96@gmail.com>
2023-01-01 20:29:56 -0600

    Add chat logo and name to load screen

--- pages/components/Loading.js
+++ pages/components/Loading.js
@@ -1,13 +1,14 @@
 import CommentIcon from '@mui/icons-material/Comment';
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
-import 'styles.css';
 
 
 function Loading() {
   return (
-    <center className='loading'>
-        <CommentIcon/>
+    <center className='loading' style={{display: 'grid', placeItems: 'center', marginTop: '30vh'}}>
+        <div className='logoIcon' style={{ fontSize: '40px', marginBottom: '20px'}}>
+            <CommentIcon/> Chattr
+        </div>
         <Box>
         <CircularProgress />
         </Box>
