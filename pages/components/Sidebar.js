ea2bc8bce7518cf91c9c0777a08be117280fc957
Faith <fhgray96@gmail.com>
2023-01-03 20:28:46 -0600

    Update new chat option to ignore an input to a chat that's already active

--- pages/components/Sidebar.js
+++ pages/components/Sidebar.js
@@ -6,12 +6,15 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import SearchIcon from '@mui/icons-material/Search';
 import { auth, db } from '../../firebase';
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollection } from 'react-firebase-hooks/firestore';
 // import EmailValidator from 'email-validator';
 
 // import * as EmailValidator from 'email-validator';
 
 function Sidebar() {
   const [user] = useAuthState(auth);
+  const userChatRef = db.collection('chats').where('users', 'array-contains', user.email);
+  const [chatsSnapshot] = useCollection(userChatRef);
 
   const createChat = () => {
     const input = prompt('Enter email address to send chat invitation');
@@ -22,12 +25,19 @@ function Sidebar() {
     //   //add chat into the DV 'chats' collection
     // }
 
-    if (input) {
+    //if there's an email input that isn't already in chat and isn't the user's email
+    if (input && !previousChat(input) && input !== user.email) {
         db.collection('chats').add({
           users: [user.email, input],
         });
     }
   };
+
+  const previousChat = (inputtedEmail) => 
+    //!! is a double bang. It makes this a boolean expression
+    //implicit return without the curly braces
+    !!chatsSnapshot?.docs.find((chat) => chat.data().users.find((user) => user === inputtedEmail)?.length > 0 
+  );
 
 
   return (
