f72c3fd702e659b19849301186a9f71c9f554a4a
Faith <fhgray96@gmail.com>
2023-01-03 21:03:20 -0600

    Add user avatar to chat listing's emails

--- pages/components/ChatListing.js
+++ pages/components/ChatListing.js
@@ -1,14 +1,26 @@
 import styled from "styled-components";
+import { Avatar } from '@mui/material';
 
 
 function ChatListing( id, users) {
   return (
     <Container>
-        <p>Chat Emails</p>
+      <UserAvatar />
+      <p>Chat Emails</p>
     </Container> 
   )
 }
 
 export default ChatListing;
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  padding: 15px;
+`;
+
+const UserAvatar = styled(Avatar)`
+  margin: 5px;
+  Margin-right: 15px;
+`;
